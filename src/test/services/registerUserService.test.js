a981687478f8822e4d6689ed31799526867a5c40
MichLadd <95909039+MichLadd@users.noreply.github.com>
2024-06-16 23:57:31 +0200

    test(UI): some service tests

---
+++ src/test/services/registerUserService.test.js
@@ -0,0 +1,34 @@
+import { register } from '../../services/registerUserService';
+import RegisterModel from '../../models/registerModel';
+import { API_URL } from '../../config';
+import fetchMock from 'jest-fetch-mock';
+
+// Enable fetch mocks
+fetchMock.enableMocks();
+
+beforeEach(() => {
+  fetchMock.resetMocks();
+});
+
+test('register › should register a user and return the user data', async () => {
+  const mockData = { id: 1, username: 'TestUser', name: 'Test', surname: 'User', role: 'Operational' };
+  fetchMock.mockResponseOnce(JSON.stringify(mockData));
+
+  const data = await register('TestUser', process.env.REACT_APP_LOGIN_TEST_PASSWORD, 'Test', 'User');
+
+  expect(fetchMock).toHaveBeenCalledWith(`${API_URL}/users/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(new RegisterModel('TestUser', process.env.REACT_APP_LOGIN_TEST_PASSWORD, 'Test', 'User', 'Operational')),
+  });
+  expect(data).toEqual(mockData);
+});
+
+test('register › should throw an error when the response is not ok', async () => {
+  const mockErrorResponse = { message: 'Error message' };
+  fetchMock.mockResponseOnce(JSON.stringify(mockErrorResponse), { status: 500 });
+
+  await expect(register('TestUser', process.env.REACT_APP_LOGIN_TEST_PASSWORD, 'Test', 'User')).rejects.toThrow('Error message');
+});
