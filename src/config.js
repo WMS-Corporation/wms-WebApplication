d2b97ad1585277773085e66d2522b9bc7068a037
MichLadd <95909039+MichLadd@users.noreply.github.com>
2024-07-02 22:58:10 +0200

    refactor(UI): default for env var

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_SERVER_URL + '/api';
-export const SERVER_URL = process.env.REACT_APP_API_SERVER_URL;
+export const SERVER_URL = process.env.REACT_APP_API_SERVER_URL || 'http://wms-gateway:3000';
+export const API_URL = SERVER_URL || + '/api';
