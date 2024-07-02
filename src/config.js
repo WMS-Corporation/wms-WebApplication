16a44a924952bff1d9eccb77665c782ff4219ca1
MichLadd <95909039+MichLadd@users.noreply.github.com>
2024-07-02 23:46:55 +0200

    chore(UI): ci

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_SERVER_URL + '/api' || 'http://wms-gateway:3000/api';
-export const SERVER_URL = process.env.REACT_APP_API_SERVER_URL || 'http://wms-gateway:3000';
+export const API_URL = process.env.REACT_APP_API_SERVER_URL + '/api' || "http://wms-gateway:3000/api";
+export const SERVER_URL = process.env.REACT_APP_API_SERVER_URL || "http://wms-gateway:3000";
